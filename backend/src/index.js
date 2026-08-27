6d68d4adbbf4d802e061399e694e71caa6f83766
Jacky <jackylai1030@gmail.com>
2026-08-27 19:09:36 -0400

    feat: configure server port using environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT
 
-app.listen(3000, () => console.log('Server is up and running on port 3000'));
+app.listen(PORT, () => console.log('Server is up and running on PORT:', PORT));
