cd32849cdeb6746ee3b1344dee5f71caeee87ac0
Jacky <jackylai1030@gmail.com>
2026-08-29 12:25:38 -0400

    feat: implement protectRoute middleware for user authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,6 @@
-export async function checkAuth(req, res,next) {
-    
+export async function checkAuth(req, res, next) {
+  if (!req.user) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  res.status(200).json(req.user);
 }
