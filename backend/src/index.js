d55d154058e3913ee0213c9bc5af477fc921cf37
Jacky <jackylai1030@gmail.com>
2026-08-29 16:03:23 -0400

    feat: integrate socket.io for real-time communication and update package dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,8 +16,7 @@ import messageRoutes from './routes/message.route.js';
 import clerkWebhook from './webhooks/clerk.webhook.js';
 
 import { connectDB } from './lib/db.js';
-
-const app = express();
+import { app, server } from './lib/socket.js';
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
@@ -52,7 +51,7 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
   console.log('Server is up and running on PORT:', PORT);
 
