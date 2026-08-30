95aee36f59c93b5a66f740d6a2214b742d8aeaf4
Jacky <jackylai1030@gmail.com>
2026-08-29 20:52:59 -0400

    feat: add PageLoader component for improved loading experience; update App component to use PageLoader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useAuth } from '@clerk/react';
 import { Navigate, Route, Routes } from 'react-router';
+import PageLoader from './components/PageLoader';
 import { ThemeProvider } from './context/ThemeContext';
 import { WallpaperProvider } from './context/WallpaperContext';
 import AuthPage from './pages/AuthPage';
@@ -8,16 +9,24 @@ import ChatPage from './pages/ChatPage';
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
 
-  if(!isLoaded) {
-    return <p>Loading...</p>;
+  if (!isLoaded) {
+    return <PageLoader />;
   }
 
   return (
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
-          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
-          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} />
+          <Route
+            path="/"
+            element={
+              isSignedIn ? <ChatPage /> : <Navigate to={'/auth'} replace />
+            }
+          />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={'/'} replace />}
+          />
         </Routes>
       </WallpaperProvider>
     </ThemeProvider>
